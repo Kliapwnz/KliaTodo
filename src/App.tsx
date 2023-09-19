44c128ba9a692134cbee0db8e1e5b0adb82a3820
Evgeniy Klimovch <kliasanik@gmail.com>
2023-09-19 14:48:57 +0300

    UseState and work delete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Components/Todolist";
 
 function App() {
-    let tasks = [
+    let [tasks, setTasks] = useState( [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "What", isDone: false}
-    ]
+    ])
     function removeTask(id:number) {
-        tasks = tasks.filter(el => el.id != id)
-        console.log(tasks)
+      let  FilteredTasks = tasks.filter(el => el.id != id)
+        setTasks(FilteredTasks)
 
     }
 
