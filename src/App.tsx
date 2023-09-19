cc4fbb1597a88120a2c66044cb97834ac4f7ccda
Evgeniy Klimovch <kliasanik@gmail.com>
2023-09-19 14:27:41 +0300

    map for todolist

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {Todolist} from "./Components/Todolist";
 
 function App() {
-    const tasks1 = [
+    const tasks = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
@@ -12,7 +12,7 @@ function App() {
 
     return (
         <div className="App">
-            <Todolist title="My Todo" task={tasks1}/>
+            <Todolist title="My Todo" task={tasks}/>
 
         </div>
     );
