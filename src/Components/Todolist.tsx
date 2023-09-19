32111c1106556a4e9bd977eb17b0385777a2ce27
Evgeniy Klimovch <kliasanik@gmail.com>
2023-09-19 15:22:35 +0300

    title input

--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FilterValuesType} from "../App";
 
 type TaskType = {
@@ -12,16 +12,20 @@ type PropsType = {
     task: TaskType[]
     removeTask: (taskId: string) => void
     changeFilter: (value: FilterValuesType) => void
-    addTask:()=>void
+    addTask: () => void
 }
 
 export const Todolist = (props: PropsType) => {
+    let [title, setTitle] = useState("")
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button onClick={()=>{props.addTask()}}>+</button>
+                <input value={title} onChange={e => setTitle(e.currentTarget.value)}/>
+                <button onClick={() => {
+                    props.addTask()
+                }}>+
+                </button>
             </div>
             <ul>
                 {props.task.map(el =>
