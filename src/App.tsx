ddc92ba0c0567552b39796305447e6eb55c0a0c7
Evgeniy Klimovch <kliasanik@gmail.com>
2023-09-19 14:27:24 +0300

    map for todolist

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,11 @@ function App() {
         {id: 3, title: "ReactJS", isDone: false},
         {id: 3, title: "What", isDone: false}
     ]
-    const tasks2 = [
-        {id: 1, title: "Hello world", isDone: true},
-        {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false}
-    ]
-    const tasks3 = [
-        {id: 1, title: "we play football", isDone: true},
-        {id: 2, title: "Man City", isDone: false},
-        {id: 3, title: "Champions", isDone: false}
-    ]
 
     return (
         <div className="App">
             <Todolist title="My Todo" task={tasks1}/>
-            <Todolist title="Second" task={tasks2}/>
-            <Todolist title="Third" task={tasks3}/>
+
         </div>
     );
 }
