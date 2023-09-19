de9257ead1ea755f4b776d9e7756e9b836a1742c
Evgeniy Klimovch <kliasanik@gmail.com>
2023-09-19 14:54:14 +0300

    tasksForTodolist and filter

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import './App.css';
 import {Todolist} from "./Components/Todolist";
 
 function App() {
-    let [tasks, setTasks] = useState( [
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "What", isDone: false}
     ])
-    function removeTask(id:number) {
-      let  FilteredTasks = tasks.filter(el => el.id != id)
+    let [filter, setFilter] = useState<"all" | "completed" | "active">("all")
+
+    let tasksForTodolist = tasks
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(el => el.isDone === false)
+    }
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(el => el.isDone === true)
+    }
+
+    function removeTask(id: number) {
+        let FilteredTasks = tasks.filter(el => el.id != id)
         setTasks(FilteredTasks)
 
     }
@@ -18,7 +28,7 @@ function App() {
     return (
         <div className="App">
             <Todolist title="My Todo"
-                      task={tasks}
+                      task={tasksForTodolist}
                       removeTask={removeTask}
             />
 
