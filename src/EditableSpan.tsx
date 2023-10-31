5c60818f5e62ecba4799db49f9b1892e7891abe3
Evgeniy Klimovch <kliasanik@gmail.com>
2023-10-31 13:15:07 +0300

    title

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -6,6 +6,7 @@ type PropsType = {
 
 export const EditableSpan = (props: PropsType) => {
     const [edit, setEdit] = useState(false)
+    const [title, setTitle] = useState(props.title)
     const editHandler = () => {
         setEdit(!edit)
     }
