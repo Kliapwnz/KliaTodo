9dfa735bccf7d0b51b2041f8524df20bc892aa2b
Evgeniy Klimovch <kliasanik@gmail.com>
2023-10-31 13:21:25 +0300

    onChangeHandler

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent, useState} from 'react';
 
 type PropsType = {
     oldTitle: string
+    onClick: (title: string) => void
 }
 
 export const EditableSpan = (props: PropsType) => {
@@ -9,9 +10,16 @@ export const EditableSpan = (props: PropsType) => {
     const [newTitle, setNewTitle] = useState(props.oldTitle)
     const editHandler = () => {
         setEdit(!edit)
+        if(edit){
+            addTask()
+        }
+
     }
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setNewTitle(e.currentTarget.value)
+    }
+    const addTask = () =>{
+        props.onClick(newTitle)
     }
     return (
         edit
