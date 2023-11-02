fe499226600db0ad6ac4353f2b8239b4188e67ff
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-02 18:41:25 +0300

    DeleteIcon

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,6 +2,8 @@ import React, {ChangeEvent} from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from "./AddItemForm";
 import {EditableSpan} from "./EditableSpan";
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export type TaskType = {
     id: string
@@ -37,14 +39,18 @@ export function Todolist(props: PropsType) {
         props.updateTodolist(props.id, newTitle)
     }
 
-    const updateTaskHandler = (tID:string ,newTitle: string) => {
+    const updateTaskHandler = (tID: string, newTitle: string) => {
         props.updateTask(props.id, tID, newTitle)
     }
 
     return <div>
         <h3>
             <EditableSpan oldTitle={props.title} onClick={updateTodolistHandler}/>
-                <button onClick={removeTodolist}>x</button>
+            <IconButton aria-label="delete"
+                        onClick={removeTodolist}
+            >
+                <DeleteIcon/>
+            </IconButton>
         </h3>
         <AddItemForm addItem={addTask}/>
         <ul>
@@ -58,8 +64,12 @@ export function Todolist(props: PropsType) {
 
                     return <li key={t.id} className={t.isDone ? "is-done" : ""}>
                         <input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>
-                        <EditableSpan oldTitle={t.title} onClick={(newTitle)=>updateTaskHandler(t.id,newTitle)}/>
-                        <button onClick={onClickHandler}>x</button>
+                        <EditableSpan oldTitle={t.title} onClick={(newTitle) => updateTaskHandler(t.id, newTitle)}/>
+                        <IconButton aria-label="delete"
+                                    onClick={onClickHandler}
+                        >
+                            <DeleteIcon/>
+                        </IconButton>
                     </li>
                 })
             }
