91d4b774122ca73053b8e136b29f0a22f4e879bb
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-02 18:12:13 +0300

    Button material ui style

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -38,7 +38,6 @@ export const AddItemForm = (props: PropsType) => {
             />
             {/*<button onClick={addItem}>+</button>*/}
             <Button variant="contained"
-                    size="small"
                     style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}
                     onClick={addItem}>+</Button>
             {error && <div className="error-message">{error}</div>}
