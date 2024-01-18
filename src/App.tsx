cbe244cc8c26278587da7f27a5bc54cb4b892e6e
Evgeniy Klimovch <kliasanik@gmail.com>
2024-01-18 14:59:34 +0300

    react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,23 @@
 import React, {useState} from 'react';
-import './App.css';
-import {Todolist} from './Todolist';
-import {v1} from 'uuid';
-
-export type FilterValuesType = "all" | "active" | "completed";
-export type TodolistsType = {
-    id: string,
-    title: string,
-    filter: FilterValuesType,
-}
+import styles from "./components/Site.module.css";
 
 
 function App() {
-
-    // let [tasks, setTasks] = useState([
-    //     {id: v1(), title: "HTML&CSS", isDone: true},
-    //     {id: v1(), title: "JS", isDone: true},
-    //     {id: v1(), title: "ReactJS", isDone: false},
-    //     {id: v1(), title: "Rest API", isDone: false},
-    //     {id: v1(), title: "GraphQL", isDone: false},
-    // ]);
-    // let [filter, setFilter] = useState<FilterValuesType>("all");
-
-    let todolistID1 = v1();
-    let todolistID2 = v1();
-
-    let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-        {id: todolistID1, title: 'What to learn', filter: 'all'},
-        {id: todolistID2, title: 'What to buy', filter: 'all'},
-    ])
-
-    let [tasks, setTasks] = useState({
-        [todolistID1]: [
-            {id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true},
-            {id: v1(), title: "ReactJS", isDone: false},
-            {id: v1(), title: "Rest API", isDone: false},
-            {id: v1(), title: "GraphQL", isDone: false},
-        ],
-        [todolistID2]: [
-            {id: v1(), title: "HTML&CSS2", isDone: true},
-            {id: v1(), title: "JS2", isDone: true},
-            {id: v1(), title: "ReactJS2", isDone: false},
-            {id: v1(), title: "Rest API2", isDone: false},
-            {id: v1(), title: "GraphQL2", isDone: false},
-        ]
-    });
-
-    function removeTask(todolistID: string, id: string) {
-        setTasks({...tasks, [todolistID]: tasks[todolistID].filter(el => el.id !== id)})
-    }
-
-    function addTask(todolistID: string, title: string) {
-        let newTask = {id: v1(), title: title, isDone: false};
-        setTasks({...tasks, [todolistID]: [newTask, ...tasks[todolistID]]});
-    }
-
-    function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
-        //     let task = tasks.find(t => t.id === taskId);
-        //     if (task) {
-        //         task.isDone = isDone;
-        //     }
-        //
-        setTasks({
-            ...tasks,
-            [todolistID]: tasks[todolistID].map(el => el.id === taskId ? {...el, isDone} : el)
-        });
-        // }
-
-    }
-
-    //
-    //
-
-
-    function changeFilter(todolistID: string, value: FilterValuesType) {
-        setTodolists(todolists.map(el => el.id === todolistID ? {...el, filter: value} : el))
-    }
-
-
     return (
-        <div className="App">
-
-            {todolists.map(el => {
-                let tasksForTodolist = tasks[el.id]
-                if (el.filter === "active") {
-                    tasksForTodolist = tasks[el.id].filter(t => !t.isDone);
-                }
-                if (el.filter === "completed") {
-                    tasksForTodolist = tasks[el.id].filter(t => t.isDone);
-                }
-                return (
-                    <Todolist
-                        key={el.id}
-                        todolistId={el.id}
-                        title={el.title}
-                        tasks={tasksForTodolist}
-                        removeTask={removeTask}
-                        addTask={addTask}
-                        changeTaskStatus={changeStatus}
-                        changeFilter={changeFilter}
-                        filter={el.filter}/>)
-            })}
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    Здесь будет навигация
+                </div>
+                <div className={styles.content}>
+                    Здесь будут кроссовки
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
+
 
 export default App;
